202c5c350182db229d5327a68a407293921fecde
MinhoJJang <ahchjang@naver.com>
2023-12-03 13:58:31 +0900

    user_based 함수 주석 추가

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -2,25 +2,43 @@ from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import numpy as np
 
+
 def user_based(table, user_id):
+
     # 2307 rows x 2810 columns
+    # Initialize a Nearest Neighbors model using cosine similarity for user-based collaborative filtering.
     knn = NearestNeighbors(metric='cosine', algorithm='brute')
+
+    # Fit the model to the data in 'table', which contains user-item interactions.
     knn.fit(table.values)
+
+    # Find the 21 nearest neighbors (including the user themselves) for each user in the dataset.
     distances, indices = knn.kneighbors(table.values, n_neighbors=21)
+
+    # Convert distance metrics to cosine similarity scores.
     cosine_similarity_ = np.array(1 - distances)
+
+    # Get the indices of 20 nearest users (excluding the user themselves) for the specified user_id.
     sim_user = indices.tolist()[table.index.tolist().index(user_id)][1:]
 
     ## weighted ratings page 25
     ## cosine_sim * visit rating / sum(cosine_sim)
-    result = np.matmul(cosine_similarity_[table.index.tolist().index(user_id)][1:],table.iloc[sim_user])\
-             /np.matmul(cosine_similarity_[table.index.tolist().index(user_id)][1:],table.iloc[sim_user]!=0)
+    # Calculate weighted ratings for recommendations.
+    # Multiply the cosine similarity of the nearest users with their ratings and normalize.
+    # This creates a weighted average based on similarity for each item.
+    result = np.matmul(cosine_similarity_[table.index.tolist().index(user_id)][1:], table.iloc[sim_user]) \
+             / np.matmul(cosine_similarity_[table.index.tolist().index(user_id)][1:], table.iloc[sim_user] != 0)
 
+    # Convert the result into a DataFrame, filling missing values with 0.
     result = pd.DataFrame(result, index=table.columns, columns=[user_id]).fillna(0)
 
+    # Save the resulting DataFrame to a CSV file for later use or analysis.
     result.to_csv("dataset/data_after_preprocessing/user_based.csv")
+
     ## Evaluation function
     ## 0이 아닌 값 : 기존에 있던 rating이랑 차이점 비교
     ## 0 : 기존에 0이였으므로 추천도가 높은거 "추천"
+
 
 def item_based(table, user_id):
     knn = NearestNeighbors(metric='cosine', algorithm='brute')
