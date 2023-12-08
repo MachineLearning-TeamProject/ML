9413e5a5c7b66771db866fd73b2677190e128694
Jihae <62400658+easyssun@users.noreply.github.com>
2023-12-08 17:18:41 +0900

    visit_area_dict 테이블 완성
    수도권 / 동부권 / 서부권 / 도서지역

--- extract_visit_area_names.py
+++ extract_visit_area_names.py
@@ -1,5 +1,6 @@
 import pandas as pd
-dataset = pd.read_csv("dataset/data_after_preprocessing/수도권/dataset.csv")
+REGION = '도서지역'
+dataset = pd.read_csv(f"dataset/data_after_preprocessing/{REGION}/dataset.csv")
 
 # dataset에서 VISIT_AREA_NM만 뽑아서 리스트 형태로 visit_area_names에 저장
 visit_area_names = dataset["VISIT_AREA_NM"].unique().tolist()
@@ -11,7 +12,7 @@ for i in range(len(dataset)):
 
 # 위 딕셔너리를 csv 파일로 저장 (utf-8로 인코딩) (csv 파일의 첫 번째 열은 key, 두 번째 열은 value로 저장)
 import csv
-with open("visit_area_dict.csv", "w", encoding="utf-8") as f:
+with open(f"visit_area_dict_{REGION}.csv", "w", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(["key", "value"])
     for key, value in visit_area_dict.items():
